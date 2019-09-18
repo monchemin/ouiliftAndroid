af259e4663db0995bb85859e03e133642f084832
nyemo <Geoleduc>
2019-09-18 00:13:58 -0400

    search new structure

--- app/src/main/java/com/ouilift/ui/search/SearchActivity.java
+++ app/src/main/java/com/ouilift/ui/search/SearchActivity.java
@@ -4,20 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+import androidx.core.widget.ContentLoadingProgressBar;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.gson.JsonObject;
 import com.ouilift.R;
 import com.ouilift.model.SearchViewModel;
+import com.ouilift.presenter.PresenterFactory;
+import com.ouilift.presenter.RouteDetailPresenter;
 import com.ouilift.ui.BaseActivity;
 import com.ouilift.ui.account.ReservationListActivity;
 import com.ouilift.ui.account.SettingsActivity;
+import com.ouilift.ui.adapter.RouteDetailAdapter;
 import com.ouilift.utils.Preference;
 
-public class SearchActivity extends BaseActivity implements SearchFragment.OnFragmentInteractionListener {
+public class SearchActivity extends BaseActivity {
 
     BottomNavigationView navView;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -33,9 +41,8 @@ public class SearchActivity extends BaseActivity implements SearchFragment.OnFra
                     startActivity(new Intent(getApplicationContext(), ReservationListActivity.class));
                     break;
                 case R.id.navigation_search:
-                    SearchFragment fragment = new SearchFragment();
-                    fragment.setViewModel(viewModel);
-                    return loadFragment(fragment);
+                    container.setVisibility(View.VISIBLE);
+                    break;
                 case R.id.navigation_setting:
                     startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
                     break;
@@ -48,6 +55,9 @@ public class SearchActivity extends BaseActivity implements SearchFragment.OnFra
     };
 
     private SearchViewModel viewModel;
+    private RouteDetailAdapter adapter;
+    private ContentLoadingProgressBar loadingIndicator;
+    LinearLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,9 +66,16 @@ public class SearchActivity extends BaseActivity implements SearchFragment.OnFra
         navView = findViewById(R.id.dashboard_nav_view);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         viewModel = ViewModelProviders.of(this).get(SearchViewModel.class);
+        RecyclerView recyclerView = findViewById(R.id.route_detail_recycler_view);
+        adapter = new RouteDetailAdapter();
+        LinearLayoutManager manager  = new LinearLayoutManager(this);
+        manager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+        loadingIndicator = findViewById(R.id.loading_indicator);
+        container = findViewById(R.id.container);
 
     }
-
 
     @Override
     protected void onResume() {
@@ -68,31 +85,31 @@ public class SearchActivity extends BaseActivity implements SearchFragment.OnFra
             findViewById(R.id.navigation_setting).setVisibility(View.INVISIBLE);
             findViewById(R.id.navigation_reservation).setVisibility(View.INVISIBLE);
         }
-        ResultFragment fragment = new ResultFragment();
-        fragment.setViewModel(viewModel);
-        fragment.setData(null, 0, 0);
-        loadFragment(fragment);
+        setData(null, 0, 0);
+
     }
 
-    private boolean loadFragment(Fragment fragment) {
-        if (fragment != null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.fragment_container, fragment)
-                    .commit();
-            return true;
-        }
-        return false;
+    private void setData(String rDate, int from, int to) {
+        loadingIndicator.show();
+        viewModel.getInternalRoute(makeJson(rDate, from, to)).observe(this, new Observer<PresenterFactory<RouteDetailPresenter>>() {
+            @Override
+            public void onChanged(PresenterFactory<RouteDetailPresenter> result) {
+
+                if (result != null) {
+                    adapter.setData(result.response);
+                }
+                loadingIndicator.hide();
+            }
+        });
+
     }
 
-    @Override
-    public void onFragmentInteraction(String rDate, int from, int to) {
-        ResultFragment fragment = new ResultFragment();
-        fragment.setViewModel(viewModel);
-        fragment.setData(rDate, from, to);
-        loadFragment(fragment);
-        navView.setVisibility(View.VISIBLE);
+    private JsonObject makeJson(String rDate, int from, int to) {
+        JsonObject data = new JsonObject();
+        data.addProperty("startDate", rDate);
+        data.addProperty("fromStation", from);
+        data.addProperty("toStation", to);
+        return data;
     }
-
 
 }
